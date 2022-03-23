20eba44ddbf518bf8e31b4a65f40683bc437eda0
cjrussell23 <cj.russell2396@gmail.com>
2022-03-23 01:46:31 -0500

    Basic functionality
    Player is a square
    Movement is random

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class Player : NetworkBehaviour
+{
+    public NetworkVariable<Vector2> Position = new NetworkVariable<Vector2>();
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            Move();
+        }
+    }
+
+    public void Move()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            var randomPosition = GetRandomPositionOnPlane();
+            transform.position = randomPosition;
+            Position.Value = randomPosition;
+        }
+        else
+        {
+            SubmitPositionRequestServerRpc();
+        }
+    }
+
+    [ServerRpc]
+    void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
+    {
+        Position.Value = GetRandomPositionOnPlane();
+    }
+
+    static Vector2 GetRandomPositionOnPlane()
+    {
+        return new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+    }
+
+    void Update()
+    {
+        transform.position = Position.Value;
+    }
+}
